fb0ccd7ee76f7a2e00601a56efad4b3f203b0ed2
Audrey Kho <ackho@uw.edu>
2021-05-14 00:54:50 -0700

    completed pt2

--- iQuiz/iQuiz/ContentView.swift
+++ iQuiz/iQuiz/ContentView.swift
@@ -6,23 +6,35 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
     @State var showingAlert: Bool = false
     @ObservedObject var categories: QuizList = QuizList()
-    
+
     var body: some View {
         NavigationView {
             List {
                 ForEach(categories.quizzes, id: \.self) { quiz in
-                    HStack (spacing: 20) {
-                        Image(systemName: quiz.icon)
-                        
-                        VStack (alignment: .leading, spacing: 5) {
-                            Text(quiz.name).font(.title2).fontWeight(.semibold)
-                            Text(quiz.desc).lineLimit(1)
-                        }
-                    }.padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    NavigationLink(
+                        destination: VStack {
+                            HStack {
+                                Text(quiz.name).font(.largeTitle).fontWeight(.semibold)
+                                Spacer()
+                            }
+                            // quiz content
+                            QuizView(items: quiz.items)
+                        }.padding(20),
+                        label: {
+                            HStack (spacing: 20) {
+                                Image(systemName: quiz.icon)
+                                
+                                VStack (alignment: .leading, spacing: 5) {
+                                    Text(quiz.name).font(.title2).fontWeight(.semibold)
+                                    Text(quiz.desc).lineLimit(1)
+                                }
+                            }.padding(.vertical, 10)
+                        })
                 }
             }.padding(.top, 5)
                 .navigationTitle("iQuiz")
@@ -45,11 +57,26 @@ class Quiz: NSObject {
     var name: String
     var desc: String
     var icon: String
+    var items: [QuizItem]
     
-    init(name: String, desc: String, icon: String) {
+    init(name: String, desc: String, icon: String, items: [QuizItem]) {
         self.name = name
         self.desc = desc
         self.icon = icon
+        self.items = items
+    }
+}
+
+class QuizItem: Identifiable {
+    let id = UUID()
+    var question: String
+    var choices: [String]
+    var answer: Int
+    
+    init(q: String, choices: [String], answer: Int) {
+        self.question = q
+        self.choices = choices
+        self.answer = answer
     }
 }
 
@@ -58,9 +85,9 @@ class QuizList: ObservableObject {
     
     init() {
         quizzes = [
-            Quiz(name: "Mathematics", desc: "Put your problem solving skills to the test", icon: "x.squareroot"),
-            Quiz(name: "Marvel Superheroes", desc: "How well do you know the heroes of the MCU?", icon: "burst"),
-            Quiz(name: "Science", desc: "Test your knowledge on scientific facts and principles", icon: "lightbulb")
+            Quiz(name: "Mathematics", desc: "Put your problem solving skills to the test", icon: "x.squareroot", items: [QuizItem(q: "What is the square root of 4?", choices: ["1", "2", "3", "4"], answer: 1)]),
+            Quiz(name: "Marvel Superheroes", desc: "How well do you know the heroes of the MCU?", icon: "burst", items: [QuizItem(q: "Who is Thor's brother?", choices: ["Odin", "Vision", "Loki", "Groot"], answer: 2), QuizItem(q: "Which infinity stone does Doctor Strange have?", choices: ["Mind", "Space", "Reality", "Time"], answer: 3), QuizItem(q: "What color is the Hulk?", choices: ["Blue", "Purple", "Green", "Yellow"], answer: 2)]),
+            Quiz(name: "Science", desc: "Test your knowledge on scientific facts and principles", icon: "lightbulb", items: [QuizItem(q: "What makes leaves green?", choices: ["cholorophyll", "bug poop", "oxygen", "the sun"], answer: 0)])
         ]
     }
 }
---
+++ iQuiz/iQuiz/QuizView.swift
@@ -0,0 +1,135 @@
+//
+//  QuizView.swift
+//  iQuiz
+//
+//  Created by stlp on 5/13/21.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    let items: [QuizItem]
+    @State var qNum: Int = 0
+    @State var scene: String = "question"
+    @State var click: Int = -1
+    @State var score: Int = 0
+    
+    var body: some View {
+        let current = items[qNum]
+        if (scene == "question") {
+            VStack {
+                Text(current.question).padding(30)
+                VStack {
+                    ForEach((0..<current.choices.count), id: \.self) { q in
+                        genButton(i: q)
+                    }
+                }
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                if (click > -1) {
+                    Button(action: {
+                        scene = "answer"
+                        if (click == current.answer) {
+                            score += 1
+                        }
+                    }, label: {
+                        Text("Submit")
+                    }).padding(20)
+                }
+            }
+        } else if (scene == "answer") {
+            VStack {
+                Text(current.question).padding(30)
+                VStack {
+                    ForEach((0..<current.choices.count), id: \.self) { q in
+                        genButton(i: q)
+                    }
+                }
+                Spacer()
+                if (click != current.answer) {
+                    Text("Wrong!").font(.title3).foregroundColor(.red)
+                    Text("The correct answer is \(current.choices[current.answer])").font(.subheadline)
+                } else {
+                    Text("Correct!").font(.title3).foregroundColor(.green)
+                }
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                if (qNum < items.count - 1) {
+                    Button(action: {
+                        scene = "question"
+                        click = -1
+                        qNum += 1
+                    }, label: {
+                        Text("Next")
+                    }).padding(20)
+                } else {
+                    Button(action: {
+                        scene = "finished"
+                    }, label: {
+                        Text("Finish")
+                    }).padding(20)
+                }
+            }
+        } else if (scene == "finished") {
+            let pct:Double = Double(score) / Double(items.count) * 100
+            Spacer()
+            VStack (spacing: 30) {
+                if (pct == 100) {
+                    Text("Perfect score!").font(.title).fontWeight(.bold).foregroundColor(.green)
+                } else if (pct >= 65) {
+                    Text("Almost!").font(.title).fontWeight(.bold).foregroundColor(.blue)
+                } else {
+                    Text("You failed!").font(.title).fontWeight(.bold).foregroundColor(.red)
+                }
+                Text("\(score) out of \(items.count) correct")
+            }
+            Spacer()
+        } else {
+            Text("Error!").foregroundColor(.red)
+        }
+    }
+    
+    func genButton(i : Int) -> some View {
+        let current = items[qNum]
+        var color : Color
+
+        if (scene == "question") {
+            if (click == i) {
+                color = .blue
+            } else {
+                color = .secondary
+            }
+        } else if (scene == "answer") {
+            if (i == current.answer && click == i) {
+                // correct ans
+                color = .green
+            } else if (click == i) {
+                color = .red
+            } else {
+                color = .secondary
+            }
+        } else {
+            color = .gray
+        }
+        return
+            Button(action: {
+                if (scene == "question") {
+                    click = i
+                }
+            }, label: {
+                Text(verbatim: current.choices[i])
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .foregroundColor(.white)
+                    .frame(width: 200, height: 50, alignment: .center)
+            })
+            .disabled(scene != "question")
+            .background(color)
+            .cornerRadius(5)
+    }
+}
